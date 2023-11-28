73a443341848e7557a125819d528247eb02b0bb4
iFlezy <mhuarcaya26@brophybroncos.org>
2023-11-28 16:30:23 -0700

    post-comp 1 #2.6: working auton (major fixes)?

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Claw.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Claw extends SubsystemBase {
+
+    DcMotor motor8;
+    Servo servo3;
+    public Claw(HardwareMap map) {
+
+        motor8 = map.get(DcMotor.class, "Claw Dc");
+        motor8.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motor8.setDirection(DcMotor.Direction.FORWARD);
+        motor8.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        servo3 = map.get(Servo.class, "Claw Servo");
+        servo3.setPosition(0);
+    }
+    public void Open() {
+
+        servo3.setPosition(0.5);
+    }
+    public void Close() {
+
+        servo3.setPosition(0);
+    }
+    public void MoveUp() {
+
+        motor8.setPower(0.5);
+    }
+    public void MoveDown() {
+
+        motor8.setPower(-0.5);
+    }
+}
